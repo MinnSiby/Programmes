6208184ec003f78a5c5c52b3c30ca3081c495da5
MinnuSiby <minnusiby5@gmail.com>
2024-09-12 15:30:59 -0400

    new files

--- StaticDemo.java
+++ StaticDemo.java
@@ -4,6 +4,7 @@ public class StaticDemo {
     String email;
     int phNo ;
     static String University = "MG University";
+    static int id = 1;
 
 
     StaticDemo(String name , String email, int phNo)
@@ -19,6 +20,7 @@ public class StaticDemo {
         System.out.println("Email is " +email);
         System.out.println("Phone number is " +phNo);
         System.out.println("University is " +University);
+        System.out.println("ID is " +id++);
     }
 
     public static void main(String[] args)
@@ -26,8 +28,11 @@ public class StaticDemo {
         StaticDemo staticDemo1 = new StaticDemo("Minnu","minnu@gmail.com", 567890123);
         staticDemo1.display();
         System.out.println();
-        StaticDemo staticDemo2 = new StaticDemo("Siby","siby@gmail.com", 456824368);
+        StaticDemo staticDemo2 = new StaticDemo("Siby","siby@gmail.com", 456824308);
         staticDemo2.display();
+        System.out.println();
+        StaticDemo staticDemo3 = new StaticDemo("Minsi","minsi@gmail.com", 256824858);
+        staticDemo3.display();
 
     }
 }
