8f73340b932a5620fbd3faf99b8cd5399085c963
MinnuSiby <minnusiby5@gmail.com>
2024-09-13 16:44:38 -0400

    new files

---
+++ StringReverse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringReverse
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the string to be reversed");
+        String name = scan.nextLine();
+        reverse(name);
+
+    }
+        public static void reverse(String name)
+        {
+            String reversed = "";
+            for (int i = name.length() - 1; i >= 0; i--) {
+                reversed = reversed + name.charAt(i);
+            }
+            System.out.println("Reversed string is : ");
+            System.out.println(reversed);
+        }
+
+
+}
